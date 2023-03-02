7e4e93c520889b7203b0bebf97f61e0abcdf233a
Arthur Sousa <61766471+arkhatic@users.noreply.github.com>
2023-03-02 11:28:00 +0000

    fix

--- src/db.js
+++ src/db.js
@@ -17,11 +17,11 @@ const firebaseConfig = {
   appId: "1:863989077253:web:d846b4b3108b199338d36c",
   measurementId: "G-17D711GFQM"
 };
-import {onLCP, onFID, onCLS} from 'web-vitals';
+//import {onLCP, onFID, onCLS} from 'web-vitals';
 
-onCLS(console.log);
-onFID(console.log);
-onLCP(console.log);
+//onCLS(console.log);
+//onFID(console.log);
+//onLCP(console.log);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
